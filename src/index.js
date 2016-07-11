1789acd9e645286a7cb68c916c87f2d0b3e0829d
komali2 <rogersjcaleb@gmail.com>
2016-07-11 15:10:16 -0700

    bp: successfully drew two shapes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-function doAThing(){
-  let x = 'hello';
-  console.log(x);
-}
+var elem = document.getElementById('draw-shapes');
+var params = {width: 285, height: 200};
+var two = new Two(params).appendTo(elem);
 
-doAThing();
+var circle = two.makeCircle(72, 100, 50);
+var rect = two.makeRectangle(213, 100, 100, 100);
+
+circle.fill = '#FF8000';
+circle.stroke = 'orangered';
+circle.linewidth = 5;
+
+rect.fill = 'rbg(0, 200, 255)';
+rect.opacity = 0.75;
+rect.noStroke();
+
+two.update();
