61ddc22e4a74caff267edd5ac2e635859537aadc
komali2 <rogersjcaleb@gmail.com>
2016-07-12 15:13:44 -0700

    core: added testing for grid class

---
+++ test/grid.spec.js
@@ -0,0 +1,59 @@
+describe('Grid Class', ()=>{
+  var x = 5;
+  var y = 5;
+  var grid = new Grid(x, y);
+  describe('Class Bits', ()=>{
+    it('should exist', ()=>{
+      expect(Grid).to.exist;
+    });
+
+    it('should be a Class', ()=>{
+      expect(Grid).to.be.a('function');
+    });
+
+    it('should allow new instances of itself', ()=>{
+      expect(grid).to.be.an.instanceof(Grid);
+    });
+  });
+
+  describe('Grid Methods', ()=>{
+
+    it('should return a matrix from its getter method', ()=>{
+      var matrix = grid.getMatrix();
+      expect(matrix).to.be.an('array');
+      expect(matrix[0]).to.be.an('array');
+    });
+
+    it('should create an array of the proper width and length', ()=>{
+      var testGrid = new Grid(2, 2);
+      var testMatrix = testGrid.getMatrix();
+      var exampleMatrix = [[1, 1], [1, 1]];
+      expect(testMatrix).to.deep.equal(exampleMatrix);
+    });
+
+    it('should return a good area', ()=>{
+      expect(grid.area).to.equal(x * y);
+    })
+
+    it('should change values with its changeCoordinateValue function', ()=>{
+      var testGrid = new Grid(2, 2);
+      var testMatrix = testGrid.getMatrix();
+      var exampleMatrix = [[1, 1], [1, 1]];
+      expect(testMatrix).to.deep.equal(exampleMatrix);
+      testGrid.changeCoordinateValue(1, 1, 2);
+      testMatrix = testGrid.getMatrix();
+      exampleMatrix = [[1, 1], [1, 2]];
+      expect(testMatrix).to.deep.equal(exampleMatrix);
+      testGrid.changeCoordinateValue(1, 0, 3);
+      testMatrix = testGrid.getMatrix();
+      exampleMatrix = [[1, 3], [1, 2]];
+      expect(testMatrix).to.deep.equal(exampleMatrix);
+    });
+
+    it('should have a printMatrix function', ()=>{
+      expect(grid.printMatrix).to.be.a('function');
+    });
+
+  });
+
+});
