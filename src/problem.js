c02db97f12226f4ad7167f2f57ca57b536e8f26b
komali2 <rogersjcaleb@gmail.com>
2016-07-20 10:07:23 -0700

    wip: first grid problem possibly drawn

--- src/problem.js
+++ src/problem.js
@@ -13,11 +13,32 @@ class Problem extends Grid{
   createExample(num){
     switch(num){
       case 1:
-        for(var i = 0; i < 10; i++){
+        //draw first row
+        for(let i = 0; i < 10; i++){
           if(i !== 0 && i !== 8){
             this.changeCoordinateValue(i, 2, WALL);
           }
         }
+        //draw second row
+        for(let i = 0; i < 10; i++){
+          if(i !== 0 && i !== 9){
+            this.changeCoordinateValue(i, 8, WALL);
+          }
+        }
+        //draw two columns
+        for(let i = 0; i < 10; i++){
+          if(i !== 0 && i !== 9){
+            this.changeCoordinateValue(1, i, WALL);
+            this.changeCoordinateValue(7, i, WALL);
+          }
+        }
+        //draw right side column
+        for(let i = 0; i <= 5; i++ ){
+          this.changeCoordinateValue(9, i, WALL);
+        }
+        this.changeCoordinateValue(8, 7, WALL);
+        this.changeCoordinateValue(8, 8, WALL);
+        this.changeCoordinateValue(9, 9, WALL);
         break;
       default:
         this.matrix = this.matrix;
