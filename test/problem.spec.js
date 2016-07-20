22d6377c78bcbd360a07b3c0393b39e080f627e3
komali2 <rogersjcaleb@gmail.com>
2016-07-20 10:12:57 -0700

    core: first tests for problem

---
+++ test/problem.spec.js
@@ -0,0 +1,22 @@
+var NONE = 1;
+var PACMAN = 2;
+var WALL = 3;
+var GOAL = 5;
+
+describe('Problem Class', ()=>{
+  var x = 10;
+  var y = 10;
+
+  var problem1 = new Problem(x, y);
+  var pacman = new Pacman(0, 0, problem1);
+
+  it('should exist', ()=>{
+    expect(Problem).to.exist;
+  });
+  it('should be a child of Grid', ()=>{
+    expect(Problem).to.be.an.instanceof(Grid);
+  });
+  it('should allow new instances of itself', ()=>{
+    expect(problem1).to.be.an.instanceof(Problem);
+  });
+});
